6552edcdf3154d47de65c5d315dcb6ed1c47e017
jason <znlswd@gmail.com>
2013-08-12 23:20:40 +0800

    yes no button is ok

--- app.js
+++ app.js
@@ -1,87 +1,6 @@
 var modelBase = "ali/"
 var width = 800
 var height = 600
-function Actor(x,y){
-	var $div = $("<div/>")
-	var $a = $("<a/>")
-	$a.attr("href","#")
-	var $img = $('<img/>')
-	$a.append($img)
-	$div.append($a);
-	$div.addClass("actor")
-	$("#canvas").append($div)
-	this.load = function(model,callback){
-		var src = modelBase+model+".gif"
-		var actor = this
-		$img.attr('src', src).load(function(){
-			var dx = actor.x+$img.width() - width
-			var dy = actor.y+$img.height() - height
-			if(dx>0){
-				actor.p().css("top",height-dy)
-				actor.y = height - dy
-			}
-			if(dy>0){
-				actor.p().css("left",width-dx)
-				actor.x = width - dx
-			}
-   			callback()
-		})
-	}
-	var tx = x || 0 
-	var ty = y || 0
-	if(tx<0){
-		tx = 0
-	}else if(tx>width){
-		tx = width	
-	}
-	if(ty<0){
-		ty = 0
-	}else if(ty>height){
-		ty = height	
-	}
-	$div.css("left",tx)
-	$div.css("top",ty)
-	this.x = tx
-	this.y = ty
-	
-	this.destroy = function(){
-		$div.remove()
-	}
-	this.top = function(t){
-		$div.animate({"top":t})
-		this.y = t
-	}
-	this.bottom = function(b){
-		$div.animate({"bottom":b})
-		this.y = height - b
-	}
-	this.left = function(l){
-		$div.animate({"left":l})
-		this.x = l
-	}
-	this.right = function(r){
-		$div.animate({"right":r})
-		this.x = width - r
-	}
-	this.move = function(dx,dy){
-		tx = this.x + (+dx)
-		ty = this.y + (+dy)
-		if(tx<0){
-			tx = 0
-		}else if(tx>width-$img.width()){
-			tx = width-$img.width()	
-		}
-		if(ty<0){
-			ty = 0
-		}else if(ty>height-$img.height()){
-			ty = height-$img.height()	
-		}
-		$div.animate({"left":tx,"top":ty})
-	}
-	this.p = function(){
-		return $div
-	}
-}
 var msgOption = {
 	show: {
 	      when: false, // Don't specify a show event
@@ -99,34 +18,6 @@ var msgOption = {
 	    name: 'cream' // Style it according to the preset 'cream' style
 	}
 }
-/*$(function(){
-	var actor1 = new Actor(800,600)
-	actor1.load("hide",function(){
-		actor1.p().on("click",e2)
-	})
-	function e2(){
-		actor1.p().unbind("click")
-		actor1.load("ride-left",function(){
-			actor1.p().animate({"left":400},{
-				"duration":5000,
-				"complete":function(){
-					actor1.load("back",e3)
-				}
-			})
-		})	
-	}
-	function e3(){
-
-	}
-	msgOption.content = "Go"
-		msgOption.position = {
-			corner: {
-	            tooltip: "bottomLeft", // Use the corner...
-	            target: "topRight" // ...and opposite corner
-	        }
-		}
-		actor1.p().qtip(msgOption)
-})*/
 $(function(){
 	$("#actor1 .action").one("click",function(){
 		var $newImg = $("<img/>").attr("src","ali/ride-left.gif").load(ride_to_mid)
@@ -143,6 +34,7 @@ $(function(){
 				$("#actor1 img").replaceWith($newImg)
 			}
 		})
+		return false;
 	}
 	function who_i_am(){
 		msgOption.content = "你想知道我是谁吗？"
@@ -164,6 +56,7 @@ $(function(){
 			$("#actor1 .action").one("click",intro2)
 		})
 		$("#actor1 img").replaceWith($newImg)
+		return false;
 	}
 	function intro2(){
 		var $newImg = $("<img/>").attr("src","ali/intro2.gif").load(function(){
@@ -177,6 +70,7 @@ $(function(){
 			$("#actor1 .action").one("click",intro3)
 		})
 		$("#actor1 img").replaceWith($newImg)
+		return false;
 	}
 	function intro3(){
 		var $newImg = $("<img/>").attr("src","ali/intro3.gif").load(function(){
@@ -190,6 +84,7 @@ $(function(){
 			$("#actor1 .action").one("click",intro4)
 		})
 		$("#actor1 img").replaceWith($newImg)
+		return false;
 	}
 	function intro4(){
 		var $newImg = $("<img/>").attr("src","ali/intro4.gif").load(function(){
@@ -203,6 +98,7 @@ $(function(){
 			$("#actor1 .action").one("click",intro5)
 		})
 		$("#actor1 img").replaceWith($newImg)
+		return false;
 	}
 	function intro5(){
 		var $newImg = $("<img/>").attr("src","ali/intro5.gif").load(function(){
@@ -216,6 +112,7 @@ $(function(){
 			$("#actor1 .action").one("click",intro6)
 		})
 		$("#actor1 img").replaceWith($newImg)
+		return false;
 	}
 	function intro6(){
 		var $newImg = $("<img/>").attr("src","ali/intro6.gif").load(function(){
@@ -229,6 +126,7 @@ $(function(){
 			$("#actor1 .action").one("click",intro7)
 		})
 		$("#actor1 img").replaceWith($newImg)
+		return false;
 	}
 	function intro7(){
 		var $newImg = $("<img/>").attr("src","ali/intro7.gif").load(function(){
@@ -242,6 +140,7 @@ $(function(){
 			$("#actor1 .action").one("click",intro8)
 		})
 		$("#actor1 img").replaceWith($newImg)
+		return false;
 	}
 	function intro8(){
 		var $newImg = $("<img/>").attr("src","ali/intro8.gif").load(function(){
@@ -255,6 +154,7 @@ $(function(){
 			$("#actor1 .action").one("click",intro9)
 		})
 		$("#actor1 img").replaceWith($newImg)
+		return false;
 	}
 	function intro9(){
 		var $newImg = $("<img/>").attr("src","ali/intro9.gif").load(function(){
@@ -265,8 +165,377 @@ $(function(){
 				target:"topRight"
 			}
 			$("#actor1").qtip(msgOption)
-			//$("#actor1 .action").one("click",intro9)
+			$("#actor1 .action").one("click",longxia0)
 		})
 		$("#actor1 img").replaceWith($newImg)
+		return false;
+	}
+	function longxia0(){
+		var $newImg = $("<img/>").attr("src","images/long-xia.jpg").load(function(){
+			$("#longxia").animate({top:0},{
+				duration:3000,
+				complete:function(){
+					$("#actor1 .action").one("click",longxia1)
+				}
+			})
+		})		
+		$("#longxia").append($newImg)
+		msgOption.content = "刷～～～～～～～"
+		$("#actor1").qtip("destroy")
+		msgOption.position.corner = {
+			tooltip:"bottomLeft",
+			target:"topRight"
+		}
+		$("#actor1").qtip(msgOption)
+		return false;
+	}
+	function longxia1(){
+		var $newImg2 = $("<img/>").attr("src","ali/rebirth.gif").load(function(){
+			msgOption.content = "版本一：复活吧！我的子民们！"
+			$("#actor1").qtip("destroy")
+			msgOption.position.corner = {
+				tooltip:"bottomRight",
+				target:"topLeft"
+			}
+			$("#actor1").qtip(msgOption)
+			$("#actor1 .action").one("click",longxia2)
+		})
+		$("#actor1 img").replaceWith($newImg2)
+		return false;
+	}
+	function longxia2(){
+		var $newImg = $("<img/>").attr("src","ali/i-am-late.gif").load(function(){
+			msgOption.content = "版本二：弟兄们，我来晚了！"
+			$("#actor1").qtip("destroy")
+			msgOption.position.corner = {
+				tooltip:"bottomLeft",
+				target:"topRight"
+			}
+			$("#actor1").qtip(msgOption)
+			$("#actor1 .action").one("click",longxia3)
+		})
+		$("#actor1 img").replaceWith($newImg)
+		return false;
+	}
+	function longxia3(){
+		var $newImg = $("<img/>").attr("src","ali/cruel.gif").load(function(){
+			msgOption.content = "好吧……这其实是一个残忍的冷笑话。"
+			$("#actor1").qtip("destroy")
+			msgOption.position.corner = {
+				tooltip:"bottomRight",
+				target:"topLeft"
+			}
+			$("#actor1").qtip(msgOption)
+			$("#actor1 .action").one("click",longxiaexit)
+		})
+		$("#actor1 img").replaceWith($newImg)
+		return false;
+	}
+	function longxiaexit(){
+		
+		$("#longxia").animate({top:-500},{
+			duration:3000,
+			complete:function(){
+				$("#actor1 .action").one("click",flyToTopLeft)
+				$("#actor1").qtip("destroy")
+			}
+		})
+		return false;
+	}
+	function flyToTopLeft(){
+		var $newImg = $("<img/>").addClass("flipx").attr("src","ali/rebirth.gif").load(function(){
+			$("#actor1").animate({left:0,top:20},{
+				duration:5000,
+				complete:toAsk
+			})			
+		})
+		$("#actor1 img").replaceWith($newImg)
+		return false;
+	}
+	function toAsk(){
+		var $newImg = $("<img/>").attr("src","ali/ask.gif").load(dashiShow)
+		$("#actor1 img").replaceWith($newImg)
+		return false;
+	}
+	function dashiShow(){
+		var $newImg = $("<img/>").attr("src","images/dashi.gif").load(function(){
+			$("#actor1 .action").one("click",function(){
+				msgOption.content = "大师，你为什么老说阿弥陀佛？"
+				msgOption.position.corner = {
+					tooltip:"topRight",
+					target:"bottomLeft"
+				}
+				$("#actor1").qtip(msgOption)
+				$("#actor2 .action").one("click",function(){
+					msgOption.content = "施主，我觉得一个出家人如果也用呵呵来表达情绪的话，这也太俗了。阿弥陀佛。"
+					msgOption.position.corner = {
+						tooltip:"topLeft",
+						target:"bottomRight"
+					}
+					$("#actor2").qtip(msgOption)
+					$("#actor1 .action").one("click",dizzy)
+				})
+			})
+		})
+		$("#actor2 img").replaceWith($newImg)
+		return false;
+	}
+	function dizzy(){
+		$("#actor1").qtip("destroy")
+		var $newImg = $("<img/>").attr("src","ali/dizzy.gif").load(function(){
+			$("#actor2 .action").one("click",function(){
+				$("#actor2").qtip("destroy")
+				$("#actor2").fadeOut({
+					complete:function(){
+						$("#actor2").remove()
+						$("#actor1 .action").one("click",freeze)
+					}
+				})
+			})
+		})
+		$("#actor1 img").replaceWith($newImg)
+		return false;
+	}
+	
+	function freeze(){
+		$("#freeze img").attr("src","images/tangsengrou.jpg").load(function(){
+			$("#freeze").fadeIn({
+				complete:function(){
+					var $newImg = $("<img/>").addClass("flipx").attr("src","ali/freeze.gif").load(function(){
+						msgOption.content = "猴哥，八戒！你们这么做对得起师傅吗？！"
+						msgOption.position.corner = {
+							tooltip:"topMiddle",
+							target:"bottomMiddle"
+						}
+						$("#actor1").qtip(msgOption)
+						$("#actor1 .action").one("click",rotate)
+					})
+					$("#actor1 img").replaceWith($newImg)
+				},
+				duration:4000
+			})
+		})
+		return false;
+	}
+	function rotate(){
+		var $newImg = $("<img/>").attr("src","ali/rotate.gif").load(function(){
+			msgOption.content = "手机在手，无奇不有。"
+			msgOption.position.corner = {
+				tooltip:"topMiddle",
+				target:"bottomMiddle"
+			}
+			$("#actor1").qtip("destroy")
+			$("#actor1").qtip(msgOption)
+			$("#actor1 .action").one("click",function(){
+				$("#freeze").fadeOut({
+					complete:mobileMaker(1,runRight)
+				})
+			})
+			function mobileMaker(time,exit){
+				if( time>0 && time<8){
+					return (function(){
+						var $div = $("<div/>").addClass("hidden").addClass("mobile")
+						var $img = $("<img/>")
+						$div.append($img)
+						$("#canvas").append($div)
+						$img.attr("src","images/mobile"+time+".jpg").load(function(){
+							$div.fadeIn({
+								duration:3000,
+								complete:function(){
+									$("#actor1 .action").one("click",function(){
+										$div.fadeOut({
+											complete:function(){
+												$("#actor1 .action").one("click",mobileMaker(time+1,exit))
+											}
+										})
+									})
+								}
+							})
+						})
+					})
+				}else{
+					return (function(){
+						var $div = $("<div/>").addClass("hidden").addClass("mobile")
+						var $img = $("<img/>")
+						$div.append($img)
+						$("#canvas").append($div)
+						$img.attr("src","images/mobile"+time+".jpg").load(function(){
+							$div.fadeIn({
+								duration:3000,
+								complete:function(){
+									$("#actor1 .action").one("click",function(){
+										$div.fadeOut({
+											complete:exit
+										})
+									})
+								}
+							})
+						})
+					})
+				}
+			}
+		})
+		$("#actor1 img").replaceWith($newImg)
+		return false;
+	}
+	function runRight(){
+		$("#actor1").qtip("destroy")
+		var $newImg = $("<img/>").attr("src","ali/run-right.gif").load(function(){
+			$("#actor1").animate({left:550},{
+				duration:3000,
+				complete:lookAround
+			})
+		})
+		$("#actor1 img").replaceWith($newImg)
+		return false;
+	}
+	function lookAround(){
+		var $newImg = $("<img/>").attr("src","ali/look-around.gif").load(function(){
+			$("#composition img").attr("src","images/composition.jpg").load(function(){
+				$("#composition").fadeIn({
+					complete:function(){
+						var $newImg = $("<img/>").attr("src","ali/happy.gif").load(function(){
+							msgOption.content = "这辈子能看到这篇作文，真值了……"
+							msgOption.position.corner = {
+								tooltip:"topMiddle",
+								target:"bottomMiddle"
+							}
+							$("#actor1").qtip(msgOption)
+							$("#actor1 .action").one("click",shame)
+						})
+						$("#actor1 img").replaceWith($newImg)
+					}
+				})
+			})
+
+		})
+		$("#actor1 img").replaceWith($newImg)
+		return false;
+	}
+	function shame(){
+		$("#actor1").qtip("destroy")
+		$("#composition").fadeOut()
+		var $newImg = $("<img/>").attr("src","ali/shame.gif").load(function(){
+			$("#actor1").animate({
+				left:720,
+				top:260
+			},{
+				duration:3000,
+				complete:miaoIntro
+			})
+		})
+		$("#actor1 img").replaceWith($newImg)
+	}
+	function miaoIntro(){
+		var $newImg = $("<img/>").attr("src","ali/miao.gif").load(function(){
+			$("#actor1").one("click",miao1)
+		})
+		$("#actor1 img").replaceWith($newImg)
+	}
+	function miao1(){
+		$("#fu img").attr("src","images/fu.jpg").load(function(){
+			$("#fu").fadeIn({
+				complete:function(){
+					var $newImg = $("<img/>").attr("src","ali/sigh.gif").load(function(){
+						msgOption.content = "对狗来说，这叫“握手”，对我来说，这叫“扶着朕！”"
+						msgOption.position.corner = {
+							tooltip:"rightMiddle",
+							target:"leftMiddle"
+						}
+						$("#actor1").qtip(msgOption)
+						$("#actor1 .action").one("click",miao2)
+					})
+					$("#actor1 img").replaceWith($newImg)
+				}
+			})
+		})
+	}
+	function miao2(){
+		$("#fu").fadeOut()
+		$("#actor1").qtip("destroy")
+		$("#dream img").attr("src","images/dog.gif").load(function(){
+			$("#dream").fadeIn({
+				complete:function(){
+					var $newImg = $("<img/>").attr("src","ali/bright.gif").load(function(){
+						msgOption.content = "这配的文字简直亮瞎了～"
+						msgOption.position.corner = {
+							tooltip:"rightMiddle",
+							target:"leftMiddle"
+						}
+						$("#actor1").qtip(msgOption)
+						$("#actor1 .action").one("click",miao3)
+					})
+					$("#actor1 img").replaceWith($newImg)
+				}
+			})
+		})
+	}
+	function miao3(){
+		$("#dream").fadeOut()
+		$("#actor1").qtip("destroy")
+		$("#scared img").attr("src","images/scared.gif").load(function(){
+			$("#scared").fadeIn({
+				complete:function(){
+					var $newImg = $("<img/>").attr("src","ali/sweet.gif").load(function(){
+						msgOption.content = "这～这喵星人吓了好几跳呢～"
+						msgOption.position.corner = {
+							tooltip:"rightMiddle",
+							target:"leftMiddle"
+						}
+						$("#actor1").qtip(msgOption)
+						$("#actor1 .action").one("click",crawlLeft)
+					})
+					$("#actor1 img").replaceWith($newImg)
+				}
+			})
+		})
+	}
+	function crawlLeft(){
+		$("#scared").fadeOut()
+		$("#actor1").qtip("destroy")
+		var $newImg = $("<img/>").attr("src","ali/crawl-left.gif").load(function(){
+			$("#actor1").animate({left:360},{
+				duration:5000,
+				complete:askLike
+			})
+		})
+		$("#actor1 img").replaceWith($newImg)
+	}
+	function askLike(){
+		var $newImg = $("<img/>").attr("src","ali/tricky.gif").load(function(){
+			msgOption.content = "怎么样，很有趣吧！"
+			msgOption.position.corner = {
+				tooltip:"topMiddle",
+				target:"bottomMiddle"
+			}
+			$("#actor1").qtip(msgOption)
+			$("#actor1 .action").one("click",function(){
+				$("#actor1").qtip("destroy")
+				var $newImg = $("<img/>").attr("src","ali/intro10.gif").load(function(){
+					msgOption.content = "那你告诉我，你喜不喜欢我？"
+					msgOption.position.corner = {
+						tooltip:"topMiddle",
+						target:"bottomMiddle"
+					}
+					$("#actor1").qtip(msgOption)
+					cantChooseNo()
+				})
+				$("#actor1 img").replaceWith($newImg)
+			})
+		})
+		$("#actor1 img").replaceWith($newImg)
+	}
+	function cantChooseNo(){
+		$(".choose").css("display","block")
+		$(".yes").one("click",function(){
+			alert("ok you right")
+		})
+		var i = 0
+		stateX = [200,50,550]
+		stateY = [500,250,10]
+		$(".no").hover(function(){
+			$(this).css("left",stateX[i]).css("top",stateY[i])
+			i = (++i) % 3
+		})
 	}
 })
